4000d448d854b95248b8011e97dd71c77893ce79
OksanaGukova <kseniagukova@ukr.net>
2024-08-28 19:11:12 +0300

    add Mongo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import { initMongoConnection } from "./bd/initMongoConnection.js";
+import { setupServer } from "./server.js";
+
+const bootstrap = async () => {
+    await initMongoConnection();
+    setupServer();
+}
+
+bootstrap();
+
 
