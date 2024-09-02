90221a4fbb4997e13067d9e5a97adf2d1fd96a3d
OksanaGukova <kseniagukova@ukr.net>
2024-09-02 22:01:47 +0300

    routs

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { getContactByIdController, getContactsController } from '../controllers/contacts.js';
+import { createContactController, deleteContactController, getContactByIdController, getContactsController, patchContactController, upsertContactController } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
  router.get('/contacts',ctrlWrapper(getContactsController));
- router.get('/contacts/:contactId',ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
  export default router;
