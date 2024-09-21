2fff65c14d29ba69d55f80012d1a51e2a4ddd137
OksanaGukova <kseniagukova@ukr.net>
2024-09-21 20:17:46 +0300

    task1

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -11,3 +11,8 @@ export const registerUserSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   });
+
+
+  export const requestResetEmailSchema = Joi.object({
+    email: Joi.string().email().required(),
+  });
